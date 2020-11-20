8371781f9fac2605ebdbc2b43feaf7aabf1a27d0
Domingo González del Castillo <domgoncas @gmail.com>
2020-11-20 19:54:42 +0100

    Entry model (Create, Edit, Show), User profile page, Initial data with Seeders

---
+++ app/Entry.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Entry extends Model
+{
+    //$entry->user
+    //Entry N - 1 User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+        
+
+}
