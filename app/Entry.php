47672f16cc0ae9f8d241927877b7cd67468dc136
Domingo González del Castillo <domgoncas @gmail.com>
2020-11-21 13:46:34 +0100

    Use friendly URLs for public Entry routes

--- app/Entry.php
+++ app/Entry.php
@@ -3,15 +3,28 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Entry extends Model
 {
     //$entry->user
     //Entry N - 1 User
+    //Eager loading
     public function user()
     {
         return $this->belongsTo(User::class);
     }
         
+    //mutator
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getUrl()
+    {
+        return url("entries/$this->slug-$this->id");
+    }
 
 }
