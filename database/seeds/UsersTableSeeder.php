679858fc8afaedbaef8823db5ee43b83866c9977
Domingo González del Castillo <domgoncas @gmail.com>
2020-11-22 12:18:05 +0100

    Use friendy URLs for users (based on username)

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,6 +14,7 @@ class UsersTableSeeder extends Seeder
     {
         User::create([
             'name' => 'Domin',
+            'username' => 'programacionymas',
             'email' => 'domgoncas@gmail.com',
             'password' => bcrypt('12345678')
         ]);
